386eb68fe16382f199dfd275fcdfb359cd1cc20f
ee <gloatingcord26@gmail.com>
2025-10-31 14:16:11 +0100

    project files commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+async function getCatFact() {
+	const response = await fetch("https://catfact.ninja/fact");
+	if (!response.ok) {
+		return;
+	}
+	console.log(response);
+
+	const data = await response.json();
+	console.log(data);
+}
+
+app.get("/", async (req, res) => {
+	const fact = await getCatFact();
+	res.render("index", {
+		title: "Hjemme side",
+		heading: "Hjemme side",
+	});
+});
+
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: "Om oss",
+		heading: "Om oss",
+	});
+});
+
+const port = 3000;
+app.listen(port, () => {
+	console.log(`Denne serveren kjører på port ${port}`);
+});
