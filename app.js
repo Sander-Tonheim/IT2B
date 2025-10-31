41ad91e0e540b1dc64bca854644530aacc7c5079
Sander Tonheim <tonheims98@gmail.com>
2025-10-31 15:19:17 +0100

    imported create connection funciton

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const { createConnection } = require("./database/database.js");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
+	const connection = await createConnection();
+	const [results] = await connection.query("SELECT * FROM car");
+	console.log(results[0].brand);
+
 	res.render("index", {
 		title: "Hjemme side",
 		heading: "Hjemme side",
